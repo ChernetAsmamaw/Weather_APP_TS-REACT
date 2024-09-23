596e34e29fb00a12ba4e7cd00cb487e178581392
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-09-23 02:10:20 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -88,7 +88,7 @@ define(['./workbox-74854057'], (function (workbox) { 'use strict';
     "revision": null
   }, {
     "url": "bundle.js",
-    "revision": "41ddc0cf37f1dea5e65a773bf71360e7"
+    "revision": "b6e8eb20fb2984ad4b8a5e1e50b8a7ff"
   }, {
     "url": "index.html",
     "revision": "2b527287bb86e898c6d86d3e590a3994"
